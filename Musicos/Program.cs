edf4ff38894cf588763e808cdeec9f793c5f1d35
MiadosC1 <l22211585@tectijuana.edu.mx>
2023-03-22 12:55:27 -0700

    Cambio 1

---
+++ Musicos/Program.cs
@@ -0,0 +1,58 @@
+﻿class Musico
+{
+    public string Nombre{get; set;}
+    public string Instrumento{get; set;}
+    public Musico(string n) => Nombre = n; //Constructor
+
+    public void Saludar(){Console.WriteLine($"Hola soy {Nombre}");}
+        
+        public virtual void Toca()
+        {
+            Console.WriteLine($"{Nombre} esta tocando su guitarra");
+        }
+}
+class Baterista:Musico
+{
+    public string Bateria {get; set;}
+    public Baterista(string Nombre, string bateria):base(Nombre)
+    {
+        Bateria = bateria;
+    }
+
+    public override void Toca()
+    {
+        Console.WriteLine($"{Nombre} tocando su bateria {Bateria}");
+    }
+
+}
+class Bajista:Musico
+{
+    public string Bajo {get; set;}
+    public Bajista(string Nombre, string bajo):base(Nombre)
+    {
+        Bajo = bajo;
+    }
+
+    public override void Toca()
+    {
+        Console.WriteLine($"{Nombre} tocando su bajo {Bajo}");
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Musico> Metallica = new List<Musico>();
+        Metallica.Add(new Musico("James Hetfield"));
+        Metallica.Add(new Baterista("Lars Ulrich", "Zildjian"));
+        Metallica.Add(new Musico("Kirk Hammet"));
+        Metallica.Add(new Bajista("Jason Newsted", "Yamaha"));
+
+        foreach (var Musico in Metallica)
+        Musico.Saludar();
+
+        foreach (var Musico in Metallica)
+        Musico.Toca();
+
+    }
+}
