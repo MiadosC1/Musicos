7e129c1fc69ae421ac599f1249d1e99ee6133c47
MiadosC1 <l22211585@tectijuana.edu.mx>
2023-03-23 12:58:56 -0700

    Update Program.cs

--- Musicos/Program.cs
+++ Musicos/Program.cs
@@ -1,15 +1,29 @@
-﻿class Musico
+﻿abstract class Musico
 {
     public string Nombre{get; set;}
     public string Instrumento{get; set;}
     public Musico(string n) => Nombre = n; //Constructor
 
-    public void Saludar(){Console.WriteLine($"Hola soy {Nombre}");}
+    public virtual void Saludar()
+    {
+        Console.WriteLine($"Hola soy {Nombre}");
+    }
         
-        public virtual void Toca()
-        {
-            Console.WriteLine($"{Nombre} esta tocando su guitarra");
-        }
+        public abstract void Toca();
+}
+
+class Guitarrista:Musico
+{
+    public string Guitarra {get; set;}
+    public Guitarrista(string Nombre, string guitarra):base(Nombre)
+    {
+        Guitarra = guitarra;
+    }
+
+    public override void Toca()
+    {
+        Console.WriteLine($"{Nombre} esta tocando su {Guitarra}");
+    }
 }
 class Baterista:Musico
 {
@@ -19,10 +33,16 @@ class Baterista:Musico
         Bateria = bateria;
     }
 
+    public override void Saludar ()
+    {
+        Console.WriteLine($"Hola {Nombre} y soy baterista");
+    }
+
     public override void Toca()
     {
         Console.WriteLine($"{Nombre} tocando su bateria {Bateria}");
     }
+
 
 }
 class Bajista:Musico
@@ -31,6 +51,11 @@ class Bajista:Musico
     public Bajista(string Nombre, string bajo):base(Nombre)
     {
         Bajo = bajo;
+    }
+
+    public override void Saludar()
+    {
+        Console.WriteLine($"Hola soy {Nombre} y soy bajista");
     }
 
     public override void Toca()
@@ -43,9 +68,9 @@ internal class Program
     private static void Main(string[] args)
     {
         List<Musico> Metallica = new List<Musico>();
-        Metallica.Add(new Musico("James Hetfield"));
+        Metallica.Add(new Guitarrista("James Hetfield", "Gibson"));
         Metallica.Add(new Baterista("Lars Ulrich", "Zildjian"));
-        Metallica.Add(new Musico("Kirk Hammet"));
+        Metallica.Add(new Guitarrista("Kirk Hammet", "Gibson"));
         Metallica.Add(new Bajista("Jason Newsted", "Yamaha"));
 
         foreach (var Musico in Metallica)
